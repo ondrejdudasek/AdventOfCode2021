43f6be42bd984bff1614070f05f07270fbf0359c
Ondřej Dudášek <ondra.dudasek@dobruska.cz>
2022-10-22 00:16:55 +0200

    Day 1 Part 1  overwritten fix

--- Day01/day01.py
+++ Day01/day01.py
@@ -3,11 +3,60 @@
 from sys import argv,exit
 from pathlib import Path
 
-"""! @brief Advent of Code 2021 Day 1 """
-
 
 def day1(filename):
     
+    print("[Part 1]")
+    increment_counter = 0
+    previous_number = None
+    current_line = 0
+
+    try:
+        file = Path(filename)
+        fd = file.open('r')
+
+        for line in fd.readlines():
+            current_line = current_line + 1
+
+            line = line.strip()
+            if(line) == '': # Line is empty
+                continue
+
+            current_number = int(line)
+
+            
+            if previous_number == None:
+                previous_number = current_number
+                #print(current_sum, 'False')
+                continue
+
+            #print(current_sum, (current_sum > previous_sum))
+            if previous_number < current_number:
+                increment_counter = increment_counter + 1
+            
+            previous_number = current_number
+
+            
+
+    except FileNotFoundError:
+        print("File not found")
+        return False
+    
+    except PermissionError:
+        print('Insufficient permissions for file')
+        return False
+    
+    except ValueError:
+        print('Invalid number on line:' + str(current_line))
+        return False
+    
+    print('Number has increased ' + str(increment_counter) + ' times.')    
+    return increment_counter
+
+def day1_p2(filename):
+    
+    print("[Part 2]")
+
     samples = []
     increment_counter = 0
     previous_sum = None
@@ -56,6 +105,7 @@ def day1(filename):
     
     except ValueError:
         print('Invalid number on line:' + str(current_line))
+        return False
     
     print('Sum has increased ' + str(increment_counter) + ' times.')    
     return increment_counter
@@ -64,8 +114,11 @@ def day1(filename):
 
 
 if argv[1] != None:
-    if not day1(argv[1]) == False:
-        exit(0)
-    exit(1)
+    if day1(argv[1]) == False:
+        exit(1)
+
+    if day1_p2(argv[1]) == False:
+        exit(1)
+    exit(0)
 
 
